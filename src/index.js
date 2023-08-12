ba5b06a4c06ed504d6c275e611ae216f51f65e75
Kirill Ivanov <hoprosx@gmail.com>
2023-08-12 10:59:02 +0500

    feat: add check func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let open = []
+    let close = []
+    
+    for(item of bracketsConfig){
+        open.push(item[0])
+        close.push(item[1])
+    }
+  
+  if(str.length % 2 != 0){
+      return false
+  }
+  
+  let openBr = []
+  
+  for(br of str){
+      if(openBr.length > 0 && close.includes(br) && open.includes(br) && (openBr[openBr.length-1] == br)){
+          openBr.pop()
+          continue
+      }
+      if(open.includes(br)){
+          openBr.push(br)
+      } else {
+          if(openBr[openBr.length -1] == open[close.indexOf(br)]){
+              openBr.pop()
+          }
+      }
+  }
+  
+  return openBr.length == 0 ? true:false
+    
+  }
